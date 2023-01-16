b95029c299ee5fa3b123eb8eced404b763d969fd
Furkan Soyturk <soyturkfurkan0@gmail.com>
2023-01-16 12:36:12 +0300

    Maintest 1&2 added

---
+++ src/testPackage1/Maintest1.java
@@ -0,0 +1,8 @@
+package testPackage1;
+
+public class Maintest1 {
+    public static void main(String[] args) {
+        System.out.println("Main branch");
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
---
+++ src/testPackage2/Maintest2.java
@@ -0,0 +1,8 @@
+package testPackage2;
+
+public class Maintest2 {
+    public static void main(String[] args) {
+        System.out.println("Master branch");
+        System.out.println(Integer.MIN_VALUE);
+    }
+}
